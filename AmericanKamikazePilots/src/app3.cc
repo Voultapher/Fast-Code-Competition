d9c8a4294d0f72b5a33d2718a6841998291f522c
Stefan Winkler <mail@stefanwinkler.de>
2017-07-25 20:29:44 +0200

    Add app3.cc

--- AmericanKamikazePilots/src/app3.cc
+++ AmericanKamikazePilots/src/app3.cc
@@ -3,10 +3,33 @@
 #include <iostream>
 #include <numeric>
 #include <vector>
+#include <deque>
+#include <mutex>
 
 void printN(int n) {
     std::async(std::launch::async, [=]{printf("%d", n);});
 }
+
+
+std::deque<int> queue;
+std::mutex mtx;
+
+void task()
+{
+    while (1) {
+        mtx.lock();
+        int i = queue.front();
+        queue.pop_front();
+        mtx.unlock();
+
+        if (i == -1)
+            return;
+
+        printf("%d", i);
+    }
+}
+
+
 
 // - Main thread creates tasks
 // - Consumer thread, accepts tasks without blocking
@@ -17,8 +40,15 @@ int main(int argc, char *argv[]) {
 
     if (argc != 2) { std::exit(1); }
 
+    std::thread t(task);
+
     const int n = std::atoi(argv[1]);
     for (int i = 0; i < n; ++i) {
-        printN(i);
+        mtx.lock();
+        queue.push_back(i);
+        mtx.unlock();
     }
+    queue.push_back(-1);
+
+    t.join();
 }
