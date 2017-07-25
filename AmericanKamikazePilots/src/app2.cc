99eee5d0d70c99f8a081ccf1f2d2d1c39844b2f3
Kai Wolf <kai.wolf@gmail.com>
2017-07-25 20:11:33 +0200

    First version of application 2

--- AmericanKamikazePilots/src/app2.cc
+++ AmericanKamikazePilots/src/app2.cc
@@ -1,5 +1,24 @@
+#include <algorithm>
+#include <future>
 #include <iostream>
+#include <numeric>
+#include <vector>
 
-int main(int argc, char* argv[]) {
-    std::cout << "hello, app2" << std::endl;
+void printN(int n) {
+    std::async([=]{printf("%d", n);}).get();
+}
+
+// - Main thread creates tasks
+// - Consumer thread, accepts tasks without blocking
+// - Run loop creating and feeding tasks N times
+// - First in First out
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) { std::exit(1); }
+
+    const int n = std::atoi(argv[1]);
+    for (int i = 0; i < n; ++i) {
+        printN(i);
+    }
 }
