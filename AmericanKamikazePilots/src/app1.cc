7ca29fd28ad641157c81503a1498bacf3e3563bf
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2017-07-25 21:14:36 +0200

    Make some stuff buildable

--- AmericanKamikazePilots/src/app1.cc
+++ AmericanKamikazePilots/src/app1.cc
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <iostream>
+#include <cstring>
 
 int main(int argc, char* argv[]) {
 
--- async-io/serial_dumb.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(int argc, char** argv) {
-    int total = atoi(argv[1]);
-    for (int i = 0; i < total; ++i)
-        cout << i;
-}
