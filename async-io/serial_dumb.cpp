5c04c029e9656a7408b9efdd7d7d67bee28a595f
Olivier Perret <olivier.perret@ppro.com>
2017-07-25 19:46:16 +0200

    aaa

--- async-io/serial_dumb.cpp
+++ async-io/serial_dumb.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main(int argc, char** argv) {
     int total = atoi(argv[1]);
-    for (int i = 0; i <= total; ++i)
+    for (int i = 1; i < total; ++i)
         cout << i << '\n';
 }
