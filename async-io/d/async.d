41f8c6f4ba7fe14c900025fc105c170eb8587322
Sebastian Wilzbach <seb@wilzba.ch>
2017-07-25 19:51:07 +0200

    First IO

--- async-io/d/async.d
+++ async-io/d/async.d
@@ -3,6 +3,20 @@
 void main(string[] args)
 {
     import std.conv, std.stdio;
+    import std.concurrency;
+    scope(failure) assert(1);
+    auto pid = spawn({
+        bool b = true;
+        while(b)
+        {
+            receive(
+                (bool a) { b = false; },
+                (int n) { n.write; }
+            );
+        }
+    });
     foreach (i; 0..args[1].to!int)
-        i.write;
+        pid.send(i);
+
+    pid.send(true);
 }
